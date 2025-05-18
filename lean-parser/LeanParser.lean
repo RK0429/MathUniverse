3516b1f49b151a41677563db4598a44b7834be0c
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-05-18 20:58:49 +0900

    REM - Eliminate several files from the `LeanParser` module, including `CollectDeps.lean`, `EntryToJson.lean`, `KindOf.lean`, `Main.lean`, and `Map.lean`. This cleanup streamlines the codebase by removing unused components, enhancing maintainability and reducing complexity in the parser's structure.

--- lean-parser/LeanParser.lean
+++ lean-parser/LeanParser.lean
@@ -1,7 +1,2 @@
 -- This module serves as the root of the `LeanParser` library.
 -- Import modules here that should be built as part of the library.
-import LeanParser.CollectDeps
-import LeanParser.EntryToJson
-import LeanParser.KindOf
-import LeanParser.Map
-import LeanParser.Main
