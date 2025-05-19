008161e222dbe9ba03e1bc6936e0f10e987de3ed
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-05-19 17:11:01 +0900

    ADD - Import additional modules `ExampleCapture` and `ExportDeps` in `LeanStmtExport.lean`, expanding the functionality of the `LeanStmtExport` library. This update enhances the module's capabilities by integrating new dependencies necessary for further development.

--- lean_stmt_export/LeanStmtExport.lean
+++ lean_stmt_export/LeanStmtExport.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `LeanStmtExport` library.
 -- Import modules here that should be built as part of the library.
-import LeanStmtExport.Basic
+import LeanStmtExport.ExampleCapture
+import LeanStmtExport.ExportDeps
